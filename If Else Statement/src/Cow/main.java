8fec9106e7ff59c61fdb084b429e19c4ea1f9aff
chrom <chrom@LAPTOP-J7I6N0JV>
2018-09-03 17:33:25 -0700

    If else statement start.

---
+++ If Else Statement/src/Cow/main.java
@@ -0,0 +1,17 @@
+package Cow;
+
+import java.util.Scanner;
+
+public class main {
+	static Scanner input = new Scanner(System.in);
+	static int age = 0;
+	static int weight = 0;
+	public static void main(String[] args) {
+		do {
+			try{
+				System.out.println("Enter your age");
+			}
+		}while(age <=0);
+
+	}
+}
