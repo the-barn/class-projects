791b233d09a7547cbded1d9839e33c9d0972f2ad
Chromefox4 <chromefox4@DESKTOP-EJINIA4>
2018-09-04 22:20:06 -0700

    Created weight function
    created weight function to work similar to age function and added
    necessary text to function

--- If Else Statement/src/Cow/main.java
+++ If Else Statement/src/Cow/main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class main {
 	static Scanner input = new Scanner(System.in);
 	static boolean ageErr = true;
-	static boolean WeightErr = true;
+	static boolean weightErr = true;
 	static int age = 0;
 	static int weight = 0;
 
@@ -16,18 +16,36 @@ public class main {
 
 	public static int age() {
 		while (ageErr) {
+			System.out.println("Enter a valid age in number form (ex: 3): ");
 			if (input.hasNextInt()) {
 				age = input.nextInt();
-				if(age <=0) {
+				if (age <= 0) {
 					continue;
 				}
-			}else{
+			} else {
 				input.next();
 				continue;
 			}
-			ageErr= false;
+			ageErr = false;
 		}
 		return age;
+	}
+
+	public static int weight() {
+		while (weightErr) {
+			System.out.println("Enter a valid weight in number form (ex: 3): ");
+			if (input.hasNextInt()) {
+				weight = input.nextInt();
+				if (weight <= 0) {
+					continue;
+				}
+			} else {
+				input.next();
+				continue;
+			}
+			weightErr = false;
+		}
+		return weight;
 	}
 
 }
